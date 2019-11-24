60f393c57af4888df96b33493b48702f9d898355
Gabriel Miranda Costa <gabrielmircosta@hotmail.com>
2019-11-24 21:09:11 -0200

    Inclusão do sistema de pagamentos e pedidos.

---
+++ Curso_Spring/cursomc/src/main/java/com/mirandagab/cursomc/domain/enums/EstadoPagamento.java
@@ -0,0 +1,34 @@
+package com.mirandagab.cursomc.domain.enums;
+
+public enum EstadoPagamento {
+
+	PENDENTE(1, "Pendente"),
+	QUITADO(2, "Quitado"),
+	CANCELADO(3, "Cancelado");
+	
+	private int cod;
+	private String descricao;
+	
+	private EstadoPagamento(int cod, String descricao) {
+		this.cod = cod;
+		this.descricao = descricao;
+	}
+	
+	public static EstadoPagamento toEnum(Integer cod) {
+		if(cod == null) return null;
+		
+		for(EstadoPagamento estadoPagamento : EstadoPagamento.values()) {
+			if(cod == estadoPagamento.getCod()) return estadoPagamento;
+		}
+		
+		throw new IllegalArgumentException("Código inválido: " + cod);
+	}
+
+	public int getCod() {
+		return cod;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+}
