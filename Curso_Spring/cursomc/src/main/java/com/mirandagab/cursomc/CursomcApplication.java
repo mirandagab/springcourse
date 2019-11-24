aba2d94b75417088f69189c6909e6e3ce235138e
Gabriel Miranda Costa <gabrielmircosta@hotmail.com>
2019-11-24 21:06:04 -0200

    Inclusão dos pedidos e pagamentos.

--- Curso_Spring/cursomc/src/main/java/com/mirandagab/cursomc/CursomcApplication.java
+++ Curso_Spring/cursomc/src/main/java/com/mirandagab/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.mirandagab.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.mirandagab.cursomc.domain.Cidade;
 import com.mirandagab.cursomc.domain.Cliente;
 import com.mirandagab.cursomc.domain.Endereco;
 import com.mirandagab.cursomc.domain.Estado;
+import com.mirandagab.cursomc.domain.Pagamento;
+import com.mirandagab.cursomc.domain.PagamentoComBoleto;
+import com.mirandagab.cursomc.domain.PagamentoComCartao;
+import com.mirandagab.cursomc.domain.Pedido;
 import com.mirandagab.cursomc.domain.Produto;
+import com.mirandagab.cursomc.domain.enums.EstadoPagamento;
 import com.mirandagab.cursomc.domain.enums.TipoCliente;
 import com.mirandagab.cursomc.repositories.CategoriaRepository;
 import com.mirandagab.cursomc.repositories.CidadeRepository;
 import com.mirandagab.cursomc.repositories.ClienteRepository;
 import com.mirandagab.cursomc.repositories.EnderecoRepository;
 import com.mirandagab.cursomc.repositories.EstadoRepository;
+import com.mirandagab.cursomc.repositories.PagamentoRepository;
+import com.mirandagab.cursomc.repositories.PedidoRepository;
 import com.mirandagab.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private ClienteRepository repoCli;
 	@Autowired
 	private EnderecoRepository repoEnd;
+	@Autowired
+	private PedidoRepository repoPedido;
+	@Autowired
+	private PagamentoRepository repoPagto;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -85,6 +97,21 @@ public class CursomcApplication implements CommandLineRunner {
 		repoCli.save(cli1);
 		repoEnd.saveAll(Arrays.asList(e1, e2));
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		repoPedido.saveAll(Arrays.asList(ped1, ped2));
+		repoPagto.saveAll(Arrays.asList(pagto1, pagto2));
 	}
 
 }
