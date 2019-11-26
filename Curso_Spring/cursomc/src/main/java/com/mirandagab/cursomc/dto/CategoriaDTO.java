2268c69b340f386c1226e2ec92f94ce58527ec5b
Gabriel Miranda Costa <gabrielmircosta@hotmail.com>
2019-11-25 23:02:52 -0200

    Inclusão de DTO para Categoria e serviço de findAll para Categoria

---
+++ Curso_Spring/cursomc/src/main/java/com/mirandagab/cursomc/dto/CategoriaDTO.java
@@ -0,0 +1,36 @@
+package com.mirandagab.cursomc.dto;
+
+import java.io.Serializable;
+
+import com.mirandagab.cursomc.domain.Categoria;
+
+public class CategoriaDTO implements Serializable {
+
+	private static final long serialVersionUID = -5832911387133505607L;
+	
+	private Integer id;
+	private String nome;
+	
+	public CategoriaDTO() {}
+	
+	public CategoriaDTO(Categoria categoria) {
+		this.id = categoria.getId();
+		this.nome = categoria.getNome();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
--- Curso_Spring/cursomc/src/main/java/com/mirandagab/cursomc/resources/CategoriaResource.java
+++ Curso_Spring/cursomc/src/main/java/com/mirandagab/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,9 @@
 package com.mirandagab.cursomc.resources;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mirandagab.cursomc.domain.Categoria;
+import com.mirandagab.cursomc.dto.CategoriaDTO;
 import com.mirandagab.cursomc.services.CategoriaService;
 
 @RestController
@@ -46,5 +50,12 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
