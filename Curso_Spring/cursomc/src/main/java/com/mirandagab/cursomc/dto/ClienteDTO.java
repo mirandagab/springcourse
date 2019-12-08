978bdd3a57707cc78f58fd257d8b45c0168923e8
mirandagab <gabrielmircosta@hotmail.com>
2019-12-08 20:31:26 -0300

    Validação de email no update do cliente

--- Curso_Spring/cursomc/src/main/java/com/mirandagab/cursomc/dto/ClienteDTO.java
+++ Curso_Spring/cursomc/src/main/java/com/mirandagab/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.mirandagab.cursomc.domain.Cliente;
+import com.mirandagab.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 
 	private static final long serialVersionUID = -5031487439217884769L;
